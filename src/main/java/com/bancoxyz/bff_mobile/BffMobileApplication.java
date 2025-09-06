dfb76e33d749633ea52a9b1af8ad0a5c50cc8e9f
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-09-06 19:29:26 -0400

    Set up main bff project

---
+++ src/main/java/com/bancoxyz/bff_mobile/BffMobileApplication.java
@@ -0,0 +1,13 @@
+package com.bancoxyz.bff_mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffMobileApplication.class, args);
+	}
+
+}
